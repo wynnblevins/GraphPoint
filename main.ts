07958aed6f0c6d48270db3bb3d02fcf2779c5270
Wynn Blevins <wynnblevins@yahoo.com>
2018-11-30 20:04:58 -0500

    Added some error handling concerning bad input values.

--- main.ts
+++ main.ts
@@ -6,16 +6,22 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question("What is point 's x value?", (pointAXRes) => {
-  rl.question("What is point A's y value?", (pointAYRes) => {
-    rl.question("What is point B's x value?", (pointBXRes) => {
-      rl.question("What is point B's y value?", (pointBYRes) => {
+rl.question("What is point A's x value? ", (pointAXRes) => {
+  rl.question("What is point A's y value? ", (pointAYRes) => {
+    rl.question("What is point B's x value? ", (pointBXRes) => {
+      rl.question("What is point B's y value? ", (pointBYRes) => {
         let pointA = new GraphPoint(pointAXRes, pointAYRes);
         let pointB = new GraphPoint(pointBXRes, pointBYRes);
         
         // calculate distance and print to screen
-        let distance = pointA.getDistance(pointB);
-        console.log(`Distance between point A and point B is ${distance}`);
+        let distance: any = pointA.getDistance(pointB);
+        
+        // check to make sure our input was valid
+        if (isNaN(distance as any)) {
+          console.log("ERROR: bad input values detected! :-(");
+        } else {
+          console.log(`Distance is ${distance.toFixed(2)}`);
+        }
 
         rl.close();
       });
